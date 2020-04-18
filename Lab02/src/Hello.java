c176a9d4288f851ad1caa6765b82eec01f059ff8
Wesley Wang <1cais@DESKTOP-1caiser>
2020-04-17 23:06:06 -0700

    Added Wesley Wang, removed extra stuff

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,21 +7,9 @@ public class Hello {
 		System.out.println("219738026 - This line was added by William Dang.");
 		System.out.println("217401198 - This line was added by Carl Viloria.");
 		System.out.println("301151396 - This line was added by Jomari Mayo.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-<<<<<<< HEAD
+		System.out.println("217028371 - This line was added by Wesley Wang.");
 		System.out.println("219736713 - This line was added by Bryan Pena.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-
-}
-
-=======
-		System.out.println("SacStateID - This line was added by First name Last name.");
 		
 	}
 	
->>>>>>> branch 'master' of https://github.com/WilliamDangCSUS/Lab04-Github.git
 }

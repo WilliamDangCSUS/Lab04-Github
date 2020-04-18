01d00c2f23bfb9c9ce44b399c5a6eebe36b456ee
Jomari M <jomarilmayo@gmail.com>
2020-04-17 19:59:24 -0700

    Added my name and deleted extra print lines

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -5,15 +5,11 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219738026 - This line was added by William Dang.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301151396 - This line was added by Jomari Mayo.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 
-}
-
+	}
+	
 }

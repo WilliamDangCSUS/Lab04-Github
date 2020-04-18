7a973198e71a4756aaf983faf3a98adc48a7fa8b
ian <ian@192.168.0.30>
2020-04-17 22:09:30 -0700

    added my ID and name on line 9

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("219738026 - This line was added by William Dang.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("217401198 - This line was added by Carl Viloria.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
